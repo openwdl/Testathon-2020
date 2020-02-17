f418a488aa3feffd2939fc0d092c7927700a63ba
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-02-17 10:10:01 +0100

    add pytest workflow test for cromwell and miniwdl

--- tests/workflow/scatter_if/test_scatter_if.py
+++ tests/workflow/scatter_if/test_scatter_if.py
@@ -1,5 +1,11 @@
-def test_scatter_if(workflow_runner):
-    """
-    if section nested within a scatter section
-    """
-    workflow_runner("test_scatter_if.wdl", {}, {"i_even": [0, 2, 4, 6, 8]})
+import json
+from pathlib import Path
+
+import pytest
+
+
+@pytest.mark.workflow("scatter_if_miniwdl")
+def test_scatter_if_miniwdl_output(workflow_dir):
+    output = Path(workflow_dir, "log.out")
+    output_dict = json.loads(output.read_text())
+    assert output_dict["outputs"]["scatter_if.i_even"] == [0, 2, 4, 6, 8]
