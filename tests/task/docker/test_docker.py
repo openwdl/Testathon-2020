6d1ff0e7ce9aa9c3998ff906bdcf3bfba6a06133
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-02-17 09:28:02 +0100

    add docker tests

--- tests/task/docker/test_docker.py
+++
@@ -1,5 +0,0 @@
-def test_optional_file_output(workflow_runner):
-    """
-    runtime.docker controls the container image in which the command runs
-    """
-    workflow_runner("test_docker.wdl", {}, {})
